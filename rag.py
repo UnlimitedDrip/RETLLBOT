383e6d30928b3f1f9417deb1ac224310a835e06e
tajsDev <tomasjauregui14@gmail.com>
2025-03-19 08:17:41 -0700

    added llm rag

---
+++ rag.py
@@ -0,0 +1,50 @@
+import sys
+import dspy
+import json
+from dotenv import load_dotenv
+load_dotenv()
+
+gpt = dspy.LM('openai/gpt-4o-mini')
+dspy.settings.configure(lm=gpt)
+
+def load_json() -> list[str]:
+    with open("/app/RETLLBOT/q_data.json", "r", encoding="utf-8") as file:
+        data = json.load(file)  
+    results = []
+    for entry in data:  
+        if isinstance(entry, dict):  
+            for key, value in entry.items():
+                if value:  
+                    results.append(str(value))  
+    return results
+
+def load_info(quest,task):
+    with open("/app/OSS-doorway/src/config/response.json", "r", encoding="utf-8") as file:
+        data = json.load(file)
+    return data[quest][task]
+
+def load_hints(quest,task):
+    with open("/app/OSS-doorway/src/config/hint_config.json", "r", encoding="utf-8") as file:
+        data = json.load(file)
+    task_hints = data.get(quest, {}).get(task, {})
+    hints = [task_hints[hint]["content"] for hint in ["H1", "H2"] if hint in task_hints]
+    return hints
+    
+class RAG(dspy.Module):
+    def __init__(self):
+        self.respond = dspy.Predict('context, question -> response')
+
+    def forward(self, question):
+        return self.respond(context=load_json(),question=question)
+
+def ragAnswer(quest,task):
+    prompt = load_info(quest,task)
+    hints = load_hints(quest,task)
+    quest = f"""Based on this task and these hints create one new
+    hint and return just that hint,task:{prompt},hints {hints}"""
+    rep = RAG()
+    return rep(question=quest).response
+    
+
+if __name__ == '__main__':
+    print(ragAnswer(sys.argv[1],sys.argv[2]))
